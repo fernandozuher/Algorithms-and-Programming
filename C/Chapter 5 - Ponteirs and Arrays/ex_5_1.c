43208b9118b570a56af0ae34250cebb2a88ced42
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-03-25 18:48:10 -0300

    Add files via upload

---
+++ C/Chapter 5 - Ponteirs and Arrays/ex_5_1.c
@@ -0,0 +1,18 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 25/03/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercise, Chapter 5 - Pointers and Arrays
+*/
+
+/*
+Exercise 5-1. As written, getint treats a + or - not followed by a digit as a valid
+representation of zero. Fix it to push such a character back on the input.
+*/
+	***I didn't understand the question.
+		if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+
+	"+ or - not followed by a digit as a valid representation of zero"
+		Actually it doesn't represent, do it?
+
