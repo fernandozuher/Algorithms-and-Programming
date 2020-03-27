e83019a963944a9771a3078cdd239702891364e9
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-03-26 21:27:46 -0300

    Add files via upload

---
+++ C/Chapters/Chapter 5 - Ponteirs and Arrays/ex_5_8.c
@@ -0,0 +1,13 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 26/03/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercise, Chapter 5 - Pointers and Arrays
+*/
+
+/*
+Exercise 5-8. There is no error checking in day_of _year or month_ day.
+Remedy this defect.
+	If there is no, ok. ;-)
+*/
