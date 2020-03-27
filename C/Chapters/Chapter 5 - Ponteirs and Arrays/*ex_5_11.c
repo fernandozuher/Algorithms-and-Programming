49995b828f9b90d7d42d0d948db647b936d53a3d
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-03-27 18:02:22 -0300

    Add files via upload

---
+++ C/Chapters/Chapter 5 - Ponteirs and Arrays/*ex_5_11.c
@@ -0,0 +1,20 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 27/03/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercise, Chapter 5 - Pointers and Arrays
+*/
+
+/*
+Page 118, PDF 132
+
+Exercise 5-11. Modify the programs entab and detab (written as exercises in
+Chapter 1) to accept a list of tab stops as arguments. Use the default tab
+settings if there are no arguments.
+*/
+	They are about the exercises 1-20 and 1-21, page 34, PDF 48.
+	I didn't do them in chapter 1 because it is not clear for me:
+	 "...to the next tab stop. Assume a fixed set of
+			tab stops, say every n columns."
+
---
+++ C/Chapters/Chapter 5 - Ponteirs and Arrays/*ex_5_12.c
@@ -0,0 +1,22 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 27/03/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercise, Chapter 5 - Pointers and Arrays
+*/
+
+/*
+Page 118, PDF 132
+
+Exercise 5-12. Extend entab and detab to accept the shorthand
+	entab -m +n
+
+to mean tab stops every n columns, starting at column m. Choose convenient
+(for the user) default behavior
+*/
+	They are about the previous exercise (5-11), 1-20 and 1-21 (chapter 1), page 34, PDF 48.
+	I didn't do them in chapter 1 because it is not clear for me:
+	 "...to the next tab stop. Assume a fixed set of
+			tab stops, say every n columns."
+
