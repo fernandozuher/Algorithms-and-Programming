8ad3a06c870116766ef7423ce5a3134560c617a3
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-03-29 16:30:34 -0300

    Add files via upload

--- C/Chapters/Chapter 5 - Ponteirs and Arrays/ex_5_14 to 5.17.c
+++ C/Chapters/Chapter 5 - Ponteirs and Arrays/ex_5_14 to 5.17.c
@@ -1,13 +1,15 @@
 /*
 Author: Fernando Zuher
 Place: Brazil
-Date: 28/03/2020
+Date: 29/03/2020
 Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
 About: Exercises, Chapter 5 - Pointers and Arrays
 */
 
 /*
 Page 121, PDF 135
+
+OBS: The sorted text is displayed after inputing an empty input (just 'enter', '\n').
 
 Exercise 5-14. Modify the sort program to handle a -r flag, which indicates
 sorting in reverse (decreasing) order. Be sure that -r works with -n
@@ -26,6 +28,34 @@ index for this book was sorted with -df for the index category and -n for the
 page numbers.)
 */
 
+/*
+// The command-line arguments can be input as '-r -n', not '-rn'.
+// But that example is the only way handled in the field-handling inside the lines.
+Look a test sample:
+./main -r -n
+
+input:
+9
+10
+8
+-df
+Fernando Zuher; xyz 4
+Fernando zuher, xyz 3
+-n
+10
+9
+8
+-d
+Fernando zuher, xyz 1
+Fernando Zuher; xyz 2
+-rn
+8
+9
+10
+
+
+*/
+
 #include <stdio.h> // fgets()
 #include <string.h> // strlen()
 #include <ctype.h> // tolower(), isalpha(), isdigit()
@@ -34,12 +64,12 @@ page numbers.)
 char *lineptr[MAXLINES]; // pointers to text lines
 
 static int options[4];
-// [0] = 'n'umeric
-// [1] = 'r'everse
-// [2] = 'f'old
-// [3] = 'd'irectory order
+	// [0] = 'n'umeric
+	// [1] = 'r'everse
+	// [2] = 'f'old
+	// [3] = 'd'irectory order
 
-int readlines(char *lineptr[], int nlines);
+int readlines(char *lineptr[], int nlines, int argc, char* argv[]);
 void writelines(char *lineptr[], int nlines);
 
 void qsort2(void *lineptr[], int left, int right,
@@ -47,10 +77,9 @@ void qsort2(void *lineptr[], int left, int right,
 
 int numcmp(char *, char *);
 
-
 int strcmp_case_insensitive(const char* s1, const char* s2); // Exercise 5-15
 int strcmp_d(const char* s1, const char* s2); // Exercise 5-16
-void options_f(int argc, char* argv[], char* line);
+void options_f(int* argc, char* argv[], char* line);
 
 // sort input lines
 int main(int argc, char* argv[])
@@ -58,23 +87,10 @@ int main(int argc, char* argv[])
 	int nlines; // number of input lines read
 
 	if (argc > 1)
-		options_f(argc, argv, NULL);
+		options_f(&argc, argv, NULL);
 
-	if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
-		if (!has_previous_line_field) {
-		
-			qsort2((void **) lineptr, 0, nlines-1, 
-			(int (*)(void*, void*))(options[3] ? strcmp_d :
-									options[2] ? strcmp_case_insensitive :
-									options[0] ? numcmp : strcmp));
-
-		}
-		else {
-			
-
-		}
-
-		printf("sorted:\n");
+	if ((nlines = readlines(lineptr, MAXLINES, argc, argv)) >= 0) {
+		printf("Sorted:\n");
 		writelines(lineptr, nlines);
 		return 0;
 
@@ -143,12 +159,11 @@ void swap(void *v[], int i, int j)
 #define MAXLEN 1000 // max length of any input line
 
 char *alloc(int);
-static int has_previous_line_field = 0;
 
 // readlines: read input lines
-int readlines(char *lineptr[], int maxlines)
+int readlines(char *lineptr[], int maxlines, int argc, char* argv[])
 {
-	int len, nlines;
+	int len, nlines, temp_lines = 0;
 	char *p, line[MAXLEN];
 	nlines = 0;
 
@@ -161,18 +176,29 @@ int readlines(char *lineptr[], int maxlines)
 			// Field-handling is considered when it is found in the beginning
 			// of a line starting by '-'.
 			if(line[0] == '-') {
-				if (!has_previous_line_field)
-					options_f(1, NULL, line);
-				else
 
+				qsort2((void **) lineptr, 0, nlines-1, 
+				(int (*)(void*, void*))(options[3] ? strcmp_d :
+								options[2] ? strcmp_case_insensitive :
+								options[0] ? numcmp : strcmp));
+
+				options_f(&argc, argv, line);
+
+				lineptr += nlines;
+				nlines = 0;
 			}
 			else {
 				strcpy(p, line);
 				lineptr[nlines++] = p;
+				temp_lines++;
 			}
 		}
 
-	return nlines;
+	qsort2((void **) lineptr, 0, nlines-1, 
+				(int (*)(void*, void*))(options[3] ? strcmp_d :
+								options[2] ? strcmp_case_insensitive :
+								options[0] ? numcmp : strcmp));
+	return temp_lines;
 }
 
 // writelines: write output lines
@@ -180,7 +206,7 @@ void writelines(char *lineptr[], int nlines)
 {
 	int i;
 	while (nlines-- > 0)
-		printf ("%s\n", *lineptr++);
+		printf ("\t%s\n", *lineptr++);
 }
 
 #define ALLOCSIZE 10000 // size of available space
@@ -239,27 +265,31 @@ int strcmp_d(const char* s1, const char* s2)
 	return *s1 - *s2;
 }
 
-void options_f(int argc, char* argv[], char* line) {
+void options_f(int* argc, char* argv[], char* line) {
 
-	if (argc > 1)
-		while (argc-- > 1) {
-			if (strcmp(*++argv, "-n") == 0) // numeric
-				options[0] = 1;
-			else if (strcmp(*argv, "-r") == 0) // reverse
-				options[1] = 1;
-			else if (strcmp(*argv, "-f") == 0) // fold
-				options[2] = 1;
-			else if (strcmp(*argv, "-d") == 0) // directory order
-				options[3] = 1;
-		} 
+	// start to initial values
+	for (int i = 0; i < 4; i++)
+		options[i] = 0;
+
+	while (*argc > 1) {
+		
+		(*argc)--;
+		*++argv;
+
+		if (strcmp( (*argv), "-n") == 0) // numeric
+			options[0] = 1;
+		else if (strcmp( (*argv), "-r") == 0) // reverse
+			options[1] = 1;
+		else if (strcmp( (*argv), "-f") == 0) // fold
+			options[2] = 1;
+		else if (strcmp( (*argv), "-d") == 0) // directory order
+			options[3] = 1;
+	}
 	
 	// If line is not NULL. Exercise 5-17.
 	// Even if the user inputs in the command-line arguments, the fields found in
 	// the lines will overwrite them.
-	if (line) {
-		// restart to initial values
-		for (int i = 0; i < 4; i++)
-			options[i] = 0;
+	if (line && *line == '-') {
 
 		// line != '\0' -->> line != 0 -->> line
 		while (*++line) {
