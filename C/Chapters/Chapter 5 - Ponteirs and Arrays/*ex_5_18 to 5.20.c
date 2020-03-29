eeb85ed77584a3f1a27ecd6d7f0665048f640e55
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-03-29 17:58:27 -0300

    Add files via upload

---
+++ C/Chapters/Chapter 5 - Ponteirs and Arrays/*ex_5_18 to 5.20.c
@@ -0,0 +1,20 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 29/03/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercises, Chapter 5 - Pointers and Arrays
+*/
+
+/*
+Page 126, PDF 140
+
+OBS: These exercises are pretty-much-very-too annoying.
+They are not-to-do exercises.
+
+Exercise 5-18. Make dcl recover from input errors.
+Exercise 5-19. Modify undcl so that it does not add redundant parentheses to 
+declarations.
+Exercise 5-20. Expand dcl to handle declarations with function argument
+types, qualifiers like const, and so on.
+*/
