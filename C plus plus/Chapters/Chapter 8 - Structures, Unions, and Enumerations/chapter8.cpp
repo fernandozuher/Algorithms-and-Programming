0acf5047377dec27f9e3927ce76b61fe3982f6cd
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-04-15 19:39:45 -0300

    Add files via upload

--- C plus plus/Chapters/Chapter 8 - Structures, Unions, and Enumerations/chapter8.cpp
+++ C plus plus/Chapters/Chapter 8 - Structures, Unions, and Enumerations/chapter8.cpp
@@ -1,7 +1,7 @@
 /*
 Author: Fernando Zuher
 Place: Brazil
-Date: 24/03/2020
+Date: 15/04/2020
 Book: The C++ Programming Language, fourth edition. Author: Bjarne Stroustrup.
 About: A summary* of code explained in the Chapter 7
                                           - Pointers, Arrays, and References
@@ -11,115 +11,110 @@ About: A summary* of code explained in the Chapter 7
 */
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int a1[10]; // 40 ints on the free store
+//////// Just Struct
+struct Address {
+	const char* name;	// "Jim Dandy"
+	int number;			// 61
+	const char* street;	// "South St"
+	const char* town;	// "New Providence"
+	char state[2];		// 'N' 'J'
+	const char* zip;	// "07974"
+};
+
+//////// Struct layout. Better declaring first larger types,
+// than smaller ones.
+// 8 bytes
+struct Readout1 {
+	int value; // 4 bytes
+	char hour; // + 1 byte
+	char seq; // + 1 byte (+ 2 bytes alignment) = 8 bytes
+};
+
+// 12 bytes
+struct Readout2 {
+	char hour; // 1 byte (+ 3 alignment)
+	int value; // + 4 bytes
+	char seq;  // 1 byte (+ 3 alignment)= 12 bytes
+};
+
+//////// Just Union
+union Value {
+	char c;
+	int i;
+};
+
+//////// Just Enumeration
+enum class Warning_INT : int { red, green, blue };
+enum class Warning_CHAR : char { red, green, blue };
+
+void print_addr(struct Address* p);
 
 int main()
 {
-////// Basic stuff
-	char c = 'a'; // char
-	char* p = &c; // ponteir to char that receives address of c;
-				  // & is the address-of operator
-	char c2 = *p; // c2 == 'a'; * is the dereference operator
+// Struct ///////////////////////////////
+	cout << "// Struct //////////////////////////////\n";
 
-	cout << c2 << endl;
+	struct Address jd = {
+		"Jim Dandy",
+		61, "South St",
+		"New Providence",
+		{'N','J'}, "07974"
+	};
 
-	int* pi; // pointer to int
-	char** ppc; // pointer to pointer to char
-	int* ap[15]; // array of 15 pointers to ints
-	int (*fp)(char*); // pointer to function taking a char* argument; returns an int
-	int* f(char*); // function taking a char* argument; returns a pointer to int
+	print_addr(&jd);
 
+	cout << "\nsizeof(struct Readout1): " << sizeof(struct Readout1);
+	cout << "\nsizeof(struct Readout2): " << sizeof(struct Readout2);
+
+
+// Union ///////////////////////////////
+	cout << "\n\n// Union //////////////////////////////" << endl;
+	Value x;
 	
-////// nullptr
-	pi = nullptr;
-	double* pd = nullptr;
-	// int i = nullptr; // error: i is not a pointer
+	x.i = 5;
+	x.c = 'a';
+	cout << "x.i = 5;" << endl << "x.c = 'a';" << endl;
+	cout << "\tx.c == " << x.c << endl;
+	cout << "\tx.i == " << x.i << " ('a' in integer is 97)" << endl;
+
+	x.c = 'a';
+	x.i = 5;
+	cout << "\nx.c = 'a';" << endl << "x.i = 5;" << endl;
+	cout << "\tx.c == " << x.c << endl;
+	cout << "\tx.i == " << x.i << endl;
 
 
-////// Arrays
-	int n;
-	cout << "Input an integer number: ";
-	cin >> n;
-	int v1[n]; // error: array size not a constant expression
-	vector<int> v2(n); // OK: vector with n int elements
+// Enum ///////////////////////////////
+	cout << "\n// Enumeration //////////////////////////////";
+	// Warning_INT a1 = 7;				// error : no int->War ning conversion
+	// int a2 = green;				// error : green not in scope
+	// int a3 = Warning_INT::green;		// error : no War ning->int conversion
+	Warning_INT a4 = Warning_INT::blue;		// OK
 
-	// above main(): int a1[10]; // 40 ints on the free store
-	int a2 [20]; // 20 ints on the stack
-	int* p2 = new int[40]; // 40 ints on the free store
+	cout << "\nsizeof(Warning_INT) == " << sizeof(Warning_INT);
+	cout << "\nsizeof(Warning_CHAR) == " << sizeof(Warning_CHAR);
 
-	
-////// Array Initializers
-	int v1_b[] = {1, 2, 3, 4};
-	char v2_b[] = {'a', 'b', 'c', 0};
-	int v5[8] = {1, 2, 3, 4}; // is equivalent to
-							  // int v5[] = {1, 2, 3, 4 , 0, 0, 0, 0};
+	cout << "\n\nstatic_cast<int>(Warning_INT::red) == " << static_cast<int>(Warning_INT::red);
+	cout << "\nstatic_cast<int>(Warning_INT::green) == " << static_cast<int>(Warning_INT::green);
+	cout << "\nstatic_cast<int>(Warning_INT::blue) == " << static_cast<int>(Warning_INT::blue);
 
-	// int v6[8] = v5; // error: can’t copy an array (cannot assign an int* to an array)
-	// v6 = v5; // error: no array assignment
+	cout << endl;
+	if (a4 == Warning_INT::red)
+		cout << "\nRED\n";
+	else
+		cout << "\nstatic_cast<int>(a4): " << static_cast<int>(a4)
+								<< " (a4 == WARNING_INT::blue)" << endl;
 
-
-////// String Literals
-	char alpha[] = "abcdefghijklmnopqrstuvwxyz"
-					"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	// it is the same "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
-////// Raw Character Strings
-	string s = "\\w\\\\w";
-	string s2 = R"(\w\\w)";
-
-	string counts {R"(1
-	22
-	333)"};
-		// is equivalent to
-		string x {"1\n22\n333"};
-
-
-////// Pointers into Arrays
-	int v[] = {1, 2, 3, 4};
-	int* p1 = v; // pointer to initial element (implicit conversion)
-	int* p2_b = &v[0]; // pointer to initial element
-	int* p3 = v+4; // pointer to one-beyond-last element
-
-
-////// Navigating Arrays
-	char v_char[] = "teste";
-	for (int i = 0; v_char[i]!=0; ++i)
-		cout << " " << i[v_char];
-	cout << "\n";
-	// "There is no inherent reason why one version should be faster than
-	// the other. With modern compilers, identical code should be
-	// (and usually is) generated for both examples. Programmers can
-	// choose between the versions on logical and aesthetic grounds."
-	for (char* p = v_char; *p!=0; ++p)
-		cout << " " << *p;
-	cout << "\n";
-
-	// a[j] == *(&a[0]+j) == *(a+j) == *(j+a) == j[a]
-
-
-////// Pointers and const
-	char *p_b;
-
-	char s_b[] = "Gorm";
-	const char* pc = s_b; // pointer to constant
-	// pc[3] = 'g'; // error: pc points to constant
-	pc = p_b; // OK
-	
-	char *const cp = s_b; // constant pointer
-	cp[3] = 'a'; // OK
-	// cp = p_b; // error: cp is constant
-	
-	const char *const cpc = s_b; // const pointer to const
-	// cpc[3] = 'a'; // error: cpc points to constant
-	// cpc = p_b; // error: cpc is constant
-	
-	char *const cp_b = p_b; // const pointer to char
-	char const* pc_b = "teste"; // pointer to const char
-	const char* pc2; // pointer to const char
-
+	cout << endl;
 	return 0;
+}
+
+void print_addr(Address* p)
+{
+	cout << p->name << '\n'
+	<< p->number << ' ' << p->street << '\n'
+	<< p->town << '\n'
+	<< p->state[0] << p->state[1] << ' ' << p->zip << '\n';
 }
