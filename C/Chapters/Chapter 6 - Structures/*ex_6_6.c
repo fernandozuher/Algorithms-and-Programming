666cceafb0b6acfdd3be833db9d2af99970261da
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-04-17 22:55:37 -0300

    Add files via upload

---
+++ C/Chapters/Chapter 6 - Structures/*ex_6_6.c
@@ -0,0 +1,19 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 17/04/2020
+Book: The C programming language, second edition. Authors: BRIAN W KERNIGHAN and DENNIS M. RITCHIE
+About: Exercise, Chapter 6 - Structures
+*/
+
+/*
+PDF 159, Page 145
+
+Exercise 6-6. Implement a simple version of the #define processor (i.e., no
+arguments) suitable for use with C programs, based on the routines of this sec-
+tion. You may also find getch and ungetch helpful.
+
+I didn't understand the question... /=
+
+*/
+
