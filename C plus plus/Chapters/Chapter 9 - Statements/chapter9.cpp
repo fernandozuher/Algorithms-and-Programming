0478240884080b330db4453d2035e12fc3ba15df
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-04-20 00:03:23 -0300

    Add files via upload

---
+++ C plus plus/Chapters/Chapter 9 - Statements/chapter9.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Fernando Zuher
+Place: Brazil
+Date: 20 April 2020
+Book: The C++ Programming Language, fourth edition. Author: Bjarne Stroustrup.
+About: A summary* of code explained in the Chapter 9 - Statements
+
+* Most of the written is taken literally from the chapter.
+
+*/
+
+#include <iostream>
+using namespace std;
+
