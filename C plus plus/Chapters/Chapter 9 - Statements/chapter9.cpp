ec321ec0f02f5bd1fdef6785b0a1452e558a110f
Fernando <37761241+fernandozuher@users.noreply.github.com>
2020-04-20 10:09:39 -0300

    Add files via upload

--- C plus plus/Chapters/Chapter 9 - Statements/chapter9.cpp
+++ C plus plus/Chapters/Chapter 9 - Statements/chapter9.cpp
@@ -3,12 +3,49 @@ Author: Fernando Zuher
 Place: Brazil
 Date: 20 April 2020
 Book: The C++ Programming Language, fourth edition. Author: Bjarne Stroustrup.
-About: A summary* of code explained in the Chapter 9 - Statements
-
-* Most of the written is taken literally from the chapter.
+About: A summary of code explained in the Chapter 9 - Statements
 
 */
 
 #include <iostream>
 using namespace std;
 
+int main()
+{
+	cout << "A programmer is a machine for turning caffeine into code."
+			" – A programmer" << endl;
+
+	int array[10] {};
+
+	cout << "\nInitialized array: ";
+	for (int x : array)
+		cout << x << " ";
+
+	cout << "\n\nModified array passed as value in for loop : ";
+	for (int x : array) {
+		x += 1;
+		cout << x << " ";
+	}
+
+	cout << "\n\nArray after 'for': ";
+	for (int x : array)
+		cout << x << " ";
+
+	cout << "\n\nModified array passed as reference in for loop: ";
+	for (int& x : array) {
+		x += 1;
+		cout << x << " ";
+	}
+
+	cout << "\n\nArray after 'for': ";
+	for (int x : array)
+		cout << x << " ";
+
+	cout << "\n\nArray iterated by begin(), end(): ";
+	for (int* i = begin(array); i != end(array); i++) {
+		cout << *i << " ";
+	}
+
+	cout << endl;
+	return 0;
+}
